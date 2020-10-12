7facb13ebee913273826d766ddff94f2a5a901dc
zetsme <zetsme182@ukr.net>
2020-10-13 01:32:31 +0300

    added useFetch

---
+++ src/components/useFetch.js
@@ -0,0 +1,16 @@
+import {useState, useEffect, useCallback} from 'react';
+
+export const useFetch = (url) => {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const getFetchedData = useCallback(() => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setFetchedData(data);
+  }, [url])
+
+  useEffect(() => {
+    getFetchedData();
+  }, [url, getFetchedData]);
+  return {fetchedData}
+}
