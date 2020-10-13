8fb0e1fecf0aab7aaa3d994e716a338744c14dd0
zetsme <zetsme182@ukr.net>
2020-10-13 03:29:05 +0300

    useFetch

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,16 +1,16 @@
-import {useState, useEffect, useCallback} from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export const useFetch = (url) => {
   const [fetchedData, setFetchedData] = useState([]);
 
-  const getFetchedData = useCallback(() => {
+  const getFetchedData = useCallback(async () => {
     const res = await fetch(url);
     const data = await res.json();
     setFetchedData(data);
-  }, [url])
+  }, [url]);
 
   useEffect(() => {
     getFetchedData();
   }, [url, getFetchedData]);
-  return {fetchedData}
-}
+  return { fetchedData };
+};
