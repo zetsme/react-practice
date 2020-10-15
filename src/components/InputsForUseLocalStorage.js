9a5fe653c536a44ae2394f6144ac851d726ea939
zetsme <zetsme182@ukr.net>
2020-10-15 04:38:39 +0300

    useLocalStorage updated

--- src/components/InputsForUseLocalStorage.js
+++ src/components/InputsForUseLocalStorage.js
@@ -1,70 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useLocalStorage from './useLocalStorage';
 
-const toCapitalize = (string) =>
-  string && string.charAt(0).toUpperCase() + string.slice(1);
-
+const cStyles = {
+  h3: 'text-2xl font-bold mb-4 underline',
+  input: 'px-2 py-1 rounded border-black border mr-4',
+  buttonAdd: 'px-2 py-1 bg-teal-700 text-white rounded',
+  box: ' bg-teal-300 p-4 rounded shadow-lg',
+  buttonRemove: 'bg-red-500 px-2 py-1 rounded text-white mt-4',
+};
 const InputsForUseLocalStorage = () => {
-  const [input, setInput] = useState('');
-  const {
-    locals: name,
-    addToLocal: addName,
-    clearLocal: removeAllNames,
-  } = useLocalStorage('name', input, setInput);
-  //
-  const [inputColors, setInputColors] = useState('');
-  const {
-    locals: colors,
-    addToLocal: addColorsToLocal,
-    clearLocal: removeAllColors,
-  } = useLocalStorage('colors', inputColors, setInputColors);
-
-  const customStyles = {
-    h3: 'text-2xl font-bold mb-4 underline',
-    input: 'px-2 py-1 rounded border-black border mr-4',
-    buttonAdd: 'px-2 py-1 bg-teal-700 text-white rounded',
-    box: ' bg-teal-300 p-4 rounded shadow-lg',
-    buttonRemove: 'bg-red-500 px-2 py-1 rounded text-white mt-4',
-  };
+  const [value, setValue] = useLocalStorage('name', '');
 
   return (
     <div className='p-4 inline-flex flex-col gap-4'>
-      <div className={customStyles.box}>
-        <input
-          className={customStyles.input}
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type='text'
-        />
-        <button onClick={addName} className={customStyles.buttonAdd}>
-          Click Me
-        </button>
-        <h3 className={customStyles.h3}>Names</h3>
-        {name ? (
-          name.map((i, idx) => <p key={idx}>{toCapitalize(i)}</p>)
-        ) : (
-          <p>Is Empty</p>
-        )}
-        <button className={customStyles.buttonRemove} onClick={removeAllNames}>
-          Remove All Names
-        </button>
-      </div>
-      <div className={customStyles.box}>
-        <input
-          className={customStyles.input}
-          value={inputColors}
-          onChange={(e) => setInputColors(e.target.value)}
-          type='text'
-        />
-        <button onClick={addColorsToLocal} className={customStyles.buttonAdd}>
-          Click Me
-        </button>
-        <h3 className={customStyles.h3}>Colors</h3>
-        {colors && colors.map((i, idx) => <p key={idx}>{toCapitalize(i)}</p>)}
-        <button className={customStyles.buttonRemove} onClick={removeAllColors}>
-          Remove All Colors
-        </button>
-      </div>
+      <InputBox value={value} setValue={setValue} name='Names' />
+    </div>
+  );
+};
+
+const InputBox = (props) => {
+  const { value, setValue, name } = props;
+  const removeItem = () => setValue('');
+  return (
+    <div className={cStyles.box}>
+      <h3 className={cStyles.h3}>{name}</h3>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className={cStyles.input}
+        type='text'
+      />
+      <button onClick={removeItem} className={cStyles.buttonRemove}>
+        Remove
+      </button>
     </div>
   );
 };
--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -1,40 +1,19 @@
 import { useEffect, useState } from 'react';
 
-const useLocalStorage = (key, newItem, clean) => {
-  const [locals, setLocals] = useState(null);
-
-  console.log('render item');
-  const addToLocal = () => {
-    setLocals((prev) => {
-      if (newItem.trim()) {
-        if (prev) {
-          localStorage.setItem(key, JSON.stringify([...prev, newItem]));
-          return [...prev, newItem];
-        } else {
-          localStorage.setItem(key, JSON.stringify([prev, newItem]));
-          return [prev, newItem];
-        }
-      } else {
-        return prev;
-      }
-    });
-    clean('');
-  };
-  const clearLocal = () => {
-    setLocals(() => {
-      localStorage.removeItem(key);
-      return null;
-    });
-  };
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(key);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+    if (typeof initialValue === 'function') {
+      return initialValue();
+    } else {
+      return initialValue;
+    }
+  });
   useEffect(() => {
-    setLocals(
-      JSON.parse(localStorage.getItem(key)) === null
-        ? []
-        : JSON.parse(localStorage.getItem(key))
-    );
-  }, []);
-
-  return { locals, addToLocal, clearLocal };
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
 };
 
 export default useLocalStorage;
